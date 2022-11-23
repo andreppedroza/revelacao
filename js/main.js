f21daec6f714249850055f395fc00125107ff981
André Pedroza <andre@pedroza.dev>
2022-11-22 21:11:14 -0300

    Edit Wheel and add Videos and Messages

--- js/main.js
+++ js/main.js
@@ -1,163 +1,209 @@
 // the game itself
-var game;
+var game
 
-var gameOptions = {
+var messages = {
+  ink: 'Amarelo é a cor da ansiedade! 🤣🤣🤣',
+  smoke: 'Só um desabafo: Queria um casal de sobrinhos!! 🥲🥲',
+  ballons: 'Vocês acharam que ia ser fácil assim? 😈'
+}
 
-    // slices (prizes) placed in the wheel
-    slices: 6,
-
-    // prize names, starting from 12 o'clock going clockwise
-    slicePrizes: [
-        "🎉 5% OFF",
-        "🎉 10% OFF",
-        "🎉 15% OFF",
-        "🎉 25% OFF",
-        "🎉 50% OFF",
-        "🎉 FREE PASTRY 🍰"
-    ],
-
-    // wheel rotation duration, in milliseconds
-    rotationTime: 3000
+var attempts = {
+  round: 0,
+  degrees: [
+    [20, 190, 349],
+    [306, 120, 96],
+    [256, 212, 40]
+  ]
 }
 
 // once the window loads...
 window.onload = function () {
+  // game configuration object
+  var gameConfig = {
+    // render type
+    type: Phaser.CANVAS,
 
-    // game configuration object
-    var gameConfig = {
+    // game width, in pixels
+    width: 850,
 
-        // render type
-        type: Phaser.CANVAS,
+    // game height, in pixels
+    height: 850,
 
-        // game width, in pixels
-        width: 850,
+    transparent: true,
 
-        // game height, in pixels
-        height: 850,
+    // scenes used by the game
+    scene: [playGame]
+  }
 
-        // game background color
-        backgroundColor: 0x880044,
+  // game constructor
+  game = new Phaser.Game(gameConfig)
 
-        // scenes used by the game
-        scene: [playGame]
-    };
-
-    // game constructor
-    game = new Phaser.Game(gameConfig);
-
-    // pure javascript to give focus to the page/frame and scale the game
-    window.focus()
-    resize();
-    window.addEventListener("resize", resize, false);
+  // pure javascript to give focus to the page/frame and scale the game
+  window.focus()
+  resize()
+  window.addEventListener('resize', resize, false)
 }
 
 // PlayGame scene
 class playGame extends Phaser.Scene {
+  // constructor
+  constructor () {
+    super('PlayGame')
+  }
 
-    // constructor
-    constructor() {
-        super("PlayGame");
-    }
+  // method to be executed when the scene preloads
+  preload () {
+    // loading assets
 
-    // method to be executed when the scene preloads
-    preload() { // loading assets
+    this.load.image('wheel', window.location.href + 'images/wheel.png')
+    this.load.image('pin', window.location.href + 'images/pin.png')
+  }
 
-        this.load.image("wheel", window.location.href + "images/wheel.png");
-        this.load.image("pin", window.location.href + "images/pin.png");
-    }
+  // method to be executed once the scene has been created
+  create () {
+    // adding the wheel in the middle of the canvas
+    this.wheel = this.add.sprite(
+      game.config.width / 2,
+      game.config.height / 2,
+      'wheel'
+    )
 
-    // method to be executed once the scene has been created
-    create() {
+    // adding the pin in the middle of the canvas
+    this.pin = this.add.sprite(
+      game.config.width / 2,
+      game.config.height / 2,
+      'pin'
+    )
 
-        // adding the wheel in the middle of the canvas
-        this.wheel = this.add.sprite(game.config.width / 2, game.config.height / 2, "wheel");
+    this.pin.setInteractive()
 
-        // adding the pin in the middle of the canvas
-        this.pin = this.add.sprite(game.config.width / 2, game.config.height / 2, "pin");
+    // adding the text field
+    this.title = this.add.text(
+      game.config.width / 2,
+      60,
+      ' Revelação do Sexo ',
+      {
+        font: '60px Bangers',
+        align: 'center',
+        color: '#022A32'
+      }
+    )
 
-        // adding the text field
-        this.prizeText = this.add.text(game.config.width / 2, game.config.height - 35, "SPIN TO WIN", {
-            font: "bold 64px Rajdhani",
-            align: "center",
-            color: "white"
-        });
+    // adding the text field
+    this.titleBorder = this.add.text(
+      game.config.width / 2 - 1,
+      59,
+      ' Revelação do Sexo ',
+      {
+        font: '60px Bangers',
+        align: 'center',
+        color: 'white'
+      }
+    )
 
-        // center the text
-        this.prizeText.setOrigin(0.5);
+    // center the text
+    this.title.setOrigin(0.5)
+    this.titleBorder.setOrigin(0.5)
 
-        // the game has just started = we can spin the wheel
-        this.canSpin = true;
+    // the game has just started = we can spin the wheel
+    this.canSpin = true
 
-        // waiting for your input, then calling "spinWheel" function
-        this.input.on("pointerdown", this.spinWheel, this);
-    }
+    // waiting for your input, then calling "spinWheel" function
+    this.pin.on('pointerdown', this.spinWheel, this)
+  }
 
-    // function to spin the wheel
-    spinWheel() {
+  // function to spin the wheel
+  spinWheel () {
+    // can we spin the wheel?
+    if (this.canSpin) {
+      // the wheel will spin round from 2 to 4 times. This is just coreography
+      var rounds = Phaser.Math.Between(4, 6)
 
-        // can we spin the wheel?
-        if (this.canSpin) {
+      // then will rotate by a random number from 0 to 360 degrees. This is the actual spin
+      // var degrees = Phaser.Math.Between(0, 360)
 
-            // resetting text field
-            this.prizeText.setText("");
+      var degrees = attempts.degrees[attempts.round][Phaser.Math.Between(0, 2)]
+      attempts.round = attempts.round >= 2 ? 0 : attempts.round + 1
 
-            // the wheel will spin round from 2 to 4 times. This is just coreography
-            var rounds = Phaser.Math.Between(4, 6);
+      // before the wheel ends spinning, we already know the prize according to "degrees" rotation and the number of slices
+      var prize = 7 - Math.floor((degrees + 28) / 60)
+      // now the wheel cannot spin because it's already spinning
+      this.canSpin = false
 
-            // then will rotate by a random number from 0 to 360 degrees. This is the actual spin
-            var degrees = Phaser.Math.Between(0, 360);
+      // animation tweeen for the spin: duration 3s, will rotate by (360 * rounds + degrees) degrees
+      // the quadratic easing will simulate friction
+      this.tweens.add({
+        // adding the wheel to tween targets
+        targets: [this.wheel],
 
-            // before the wheel ends spinning, we already know the prize according to "degrees" rotation and the number of slices
-            var prize = gameOptions.slices - 1 - Math.floor(degrees / (360 / gameOptions.slices));
+        // angle destination
+        angle: 360 * rounds + degrees,
 
-            // now the wheel cannot spin because it's already spinning
-            this.canSpin = false;
+        // tween duration
+        duration: 3000,
 
-            // animation tweeen for the spin: duration 3s, will rotate by (360 * rounds + degrees) degrees
-            // the quadratic easing will simulate friction
-            this.tweens.add({
+        // tween easing
+        ease: 'Cubic.easeOut',
 
-                // adding the wheel to tween targets
-                targets: [this.wheel],
+        // callback scope
+        callbackScope: this,
 
-                // angle destination
-                angle: 360 * rounds + degrees,
-
-                // tween duration
-                duration: gameOptions.rotationTime,
-
-                // tween easing
-                ease: "Cubic.easeOut",
-
-                // callback scope
-                callbackScope: this,
-
-                // function to be executed once the tween has been completed
-                onComplete: function (tween) {
-                    // displaying prize text
-                    this.prizeText.setText(gameOptions.slicePrizes[prize]);
-
-                    // player can spin again
-                    this.canSpin = false;
-                }
-            });
+        // function to be executed once the tween has been completed
+        onComplete: function (tween) {
+          var result = ['ink', 'ballons', 'smoke'][prize % 3]
+          var video = document.getElementById(result)
+          if (video) {
+            video.classList.remove('hide')
+            video.play().then(() => {
+              setTimeout(() => {
+                document.getElementById(
+                  'message-container'
+                ).className = `base fadin ${result}-color`
+                setTimeout(() => {
+                  typeMessage(messages[result])
+                  video.classList.add('hide')
+                  video.pause()
+                  video.currentTime = 0
+                }, 1000)
+                this.canSpin = true
+              }, 5000)
+            })
+          }
         }
+      })
     }
+  }
 }
 
 // pure javascript to scale the game
-function resize() {
-    var canvas = document.querySelector("canvas");
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
-    var windowRatio = windowWidth / windowHeight;
-    var gameRatio = game.config.width / game.config.height;
-    if (windowRatio < gameRatio) {
-        canvas.style.width = windowWidth + "px";
-        canvas.style.height = (windowWidth / gameRatio) + "px";
-    }
-    else {
-        canvas.style.width = (windowHeight * gameRatio) + "px";
-        canvas.style.height = windowHeight + "px";
-    }
+function resize () {
+  var canvas = document.querySelector('canvas')
+  var windowWidth = window.innerWidth
+  var windowHeight = window.innerHeight
+  var windowRatio = windowWidth / windowHeight
+  var gameRatio = game.config.width / game.config.height
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + 'px'
+    canvas.style.height = windowWidth / gameRatio + 'px'
+  } else {
+    canvas.style.width = windowHeight * gameRatio + 'px'
+    canvas.style.height = windowHeight + 'px'
+  }
+}
+
+function typeMessage (msg, i = 0) {
+  if (i < msg.length) {
+    document.getElementById('message').innerHTML += msg.charAt(i)
+    i++
+    setTimeout(() => typeMessage(msg, i++), 50)
+  } else {
+    var container = document.getElementById('message-container')
+    setTimeout(() => {
+      container.classList.add('fadout')
+      setTimeout(() => {
+        document.getElementById('message').innerHTML = ''
+        container.classList.add('hide')
+      }, 1000)
+    }, 5000)
+  }
 }
